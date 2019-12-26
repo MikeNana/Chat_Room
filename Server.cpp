c229d5adb4b3583e5ab7879abbd96f8ea3def724
admin <MikeNana@email.com>
2019-12-26 21:24:35 +0800

    Chat-Room version-2.0

--- Server.cpp
+++ Server.cpp
@@ -1,6 +1,10 @@
 //服务器设计的基本步骤:socket->bind->listen->accept
+
+//版本二:实现多个用户连接服务器端
+//服务器需要完成的主要功能接收新的连接和消息转发
 #include <sys/socket.h>   //connect,send,recv,setsockopt等
 #include <sys/types.h>      
+#include <sys/epoll.h>
 
 #include <netinet/in.h>     // sockaddr_in, "man 7 ip" ,htons
 #include <poll.h>             //poll,pollfd
@@ -11,88 +15,130 @@
 #include <error.h>         //perror
 #include <stdio.h>
 #include <errno.h>         //errno
-
+#include <signal.h>         //sigaction
 #include <string.h>          // memset
 #include <string>
 #include <iostream>
 
+#include <vector>
+#include <string>
+#include <set>
 #define port 8000
+#define LISTENQ 5
+#define MAX_EVENTS 20
+#define MAX_MESSAGE 10000
 
 using namespace std;
-
-static const int MAX_MESSAGE = 10000;
-void handle_error(const char* message)
+//打开SO_REUSEADDR设置，避免服务器重启时端口还未释放
+static const int on = 1;
+void error_handler(const char* str)
 {
-    fputs(message, stderr);
-    fputc('\n', stderr);
+    printf(" %s ", str);
     exit(1);
 }
 
-int main(int argc, char** argv)
+//对SIGPIPE设置处理函数
+void handle_for_sigpipe()
 {
-    if(argc != 2){ handle_error(" usage ./Server <port>" ); }
+    struct sigaction sa;
+    memset(&sa, '\0', sizeof(sa));
+    sa.sa_handler = SIG_IGN;
+    sa.sa_flags = 0;
+    if(sigaction(SIGPIPE, &sa, NULL))
+        return ;
+}
 
-    int listenfd;
-    listenfd = socket(AF_INET, SOCK_STREAM, 0);
-    if(listenfd < 0){ handle_error(" listen failed.\n" ); }
 
-//初始化服务端地址结构
-    struct sockaddr_in serv_addr;
+int main(int argc, char* argv[])
+{
+    int listen_fd, conn_fd, epoll_fd;
+    struct sockaddr_in serv_addr, cli_addr;
+    struct epoll_event events[MAX_EVENTS], ev;
+    socklen_t cliaddr_len;
+
+    int nfds = 0;
+    int read_len = 0;
+    char buf[MAX_MESSAGE] = {0};
+    char message_recv[MAX_MESSAGE] = {0};
+    set<int> fd_set;
+//创建socket
+    if((listen_fd = socket(AF_INET, SOCK_STREAM, 0)) < 0)
+        error_handler("socket failed.\n");
+//将listenfd设置为reuse，避免重启时端口被占用未被释放
+    setsockopt(listen_fd, SOL_SOCKET, SO_REUSEADDR, &on, sizeof(on));
+//固定服务器地址
     bzero(&serv_addr, sizeof(serv_addr));
+    serv_addr.sin_addr.s_addr = INADDR_ANY;
     serv_addr.sin_family = AF_INET;
     serv_addr.sin_port = htons(port);
-    serv_addr.sin_addr.s_addr = htonl(INADDR_ANY);
-//绑定
-    if(bind(listenfd, (struct sockaddr*)&serv_addr, sizeof(serv_addr)) < 0)
-    {
-        handle_error("bind error");
-    }
-//监听
-    if(listen(listenfd, 3) < 0)
-    {
-        handle_error("listen failed");
-        close(listenfd);
-    }
+//绑定监听端口
+    if(bind(listen_fd, (struct sockaddr*)&serv_addr, sizeof(serv_addr)) < 0)
+        error_handler("bind failed.\n");
+//启动监听
+    if(listen(listen_fd, LISTENQ) < 0)
+        error_handler("listen failed.\n");
+//创建epoll_fd并将listen事件注册到epoll中
+    epoll_fd = epoll_create(MAX_EVENTS);
+    ev.data.fd = listen_fd;
+    ev.events = EPOLLIN | EPOLLHUP;
+    epoll_ctl(epoll_fd, EPOLL_CTL_ADD, listen_fd, &ev);
 
-    cout << "after listen..." << endl;
-    
-    while(1)
+    handle_for_sigpipe();
+
+//开始epoll轮询，并分类处理各类事件
+    while(true)
     {
-        struct sockaddr_in cli_addr;
-        socklen_t cliaddr_len = sizeof(cli_addr);
-        int message_len = 0;
-        int read_len = 0;
-        int connfd;
-        char message_recv[MAX_MESSAGE] = {0};
-        char message_send[MAX_MESSAGE] = {0};
-        char buf[MAX_MESSAGE] = {0};
-        cout << "before accept" << endl;
-//接受连接请求
-        if((connfd = accept(listenfd, (struct sockaddr*)&cli_addr, &cliaddr_len)) < 0)
+//暂时将事件返回时间设置为0
+        nfds = epoll_wait(epoll_fd, events, MAX_EVENTS, 0);
+        for(int i = 0; i < nfds; ++i)
         {
-            perror("accept failed");
-            continue;
-        }
-        else
-        {
-            printf("client %s connected.\n", inet_ntoa(cli_addr.sin_addr));
-        }
-        
-//接收数据并echo
-        while(1)
-        {
-            if((read_len = read(connfd, buf, MAX_MESSAGE)) < 0)
+//如果是新事件的到来，则需要接受connect请求，存储该连接的connfd
+            if(events[i].data.fd == listen_fd)
             {
-                perror("read failed\n");
-                break;
+                int cur_connfd;
+                if((cur_connfd = accept(listen_fd, (struct sockaddr*)&cli_addr, &cliaddr_len)) < 0)
+                    error_handler("accept failed.\n");
+                cout << "accept a new connection from " << inet_ntoa(cli_addr.sin_addr) << endl;
+                ev.data.fd = cur_connfd;
+                ev.events = EPOLLIN | EPOLLET;
+                epoll_ctl(epoll_fd, EPOLL_CTL_ADD, cur_connfd, &ev);
+                fd_set.insert(cur_connfd);
             }
-            printf("message from client %s: %s",inet_ntoa(cli_addr.sin_addr), buf);
-            fgets(buf, MAX_MESSAGE, stdin);
-            write(connfd, buf, read_len);
-            memset(buf, 0, sizeof(buf));
+//如果是某连接可读
+            else if (events[i].events & EPOLLIN)
+            {
+                int cur_connfd = events[i].data.fd;
+                const char* cli_ip = inet_ntoa(cli_addr.sin_addr);
+                strcat(buf, cli_ip);
+                strcat(buf, ": ");
+                if((read_len = read(cur_connfd, message_recv, MAX_MESSAGE)) < 0)
+                    error_handler("read failed.\n");
+                strcat(buf, message_recv);
+                printf("%s", buf);
+//并将该信息转发给其他所有的连接
+                for(auto a:fd_set)
+                {
+                    if(write(a, buf, MAX_MESSAGE) < 0)
+                    {
+                        cout << "write error" << endl;
+                        fd_set.erase(a);
+                    }
+                    else
+                    {
+                        cout << "write success" << endl;
+                    }
+                    
+                }
+                memset(buf, '\0', sizeof(buf));
+                printf("%s", buf);
+            }
         }
-        close(connfd);
     }
-    close(listenfd);
+    if(!fd_set.empty())
+    {
+        for(auto a : fd_set)
+            close(a);
+    }
+    close(listen_fd);
     return 0;
 }
--- client.cpp
+++ client.cpp
@@ -1,12 +1,14 @@
+//客户端也要用epoll，否则无法快速读取其他用户发送的信息
 #include <sys/socket.h>   //connect,send,recv,setsockopt等
 #include <sys/types.h>      
 
 #include <netinet/in.h>     // sockaddr_in, "man 7 ip" ,htons
 #include <poll.h>             //poll,pollfd
+#include <sys/epoll.h>
 #include <arpa/inet.h>   //inet_addr,inet_aton
 #include <unistd.h>        //read,write
 #include <netdb.h>         //gethostbyname
-
+#include <fcntl.h>
 #include <error.h>         //perror
 #include <stdio.h>
 #include <errno.h>         //errno
@@ -16,14 +18,29 @@
 #include <iostream>
 
 #define port 8000
+#define MAX_EVENTS 10
 using namespace std;
 
 static const int MAX_MESSAGE = 10000;
+//错误处理函数
 void handle_error(const char* message)
 {
     fputs(message, stderr);
     fputc('\n', stderr);
     exit(1);
+}
+
+//设置非阻塞模式函数
+int set_nonblock(int fd)
+{
+    int flags;
+    flags = fcntl(fd, F_GETFL);
+    if(flags < 0)
+        return flags;
+    flags |= O_NONBLOCK;
+    if(fcntl(fd, F_SETFL, flags) < 0)
+        return -1;
+    return 0;
 }
 
 int main(int argc, char** argv)
@@ -33,17 +50,20 @@ int main(int argc, char** argv)
         perror("argument error.");
         exit(-1);
     }
-    int connfd;
+    int connfd, epoll_fd;
     int message_len = 0;
     int read_len = 0, write_len = 0;
-    char message[MAX_MESSAGE] = {0};
+    int nfds = 0;
+    char message_send[MAX_MESSAGE] = {0};
+    char message_recv[MAX_MESSAGE] = {0};
     struct sockaddr_in serv_addr;
     socklen_t servaddr_len = sizeof(serv_addr);
-
+    struct epoll_event ev, events[MAX_EVENTS];
     if((connfd = socket(AF_INET, SOCK_STREAM, 0)) < 0)
     {
         handle_error("socket error\n");
     }
+    
     bzero(&serv_addr, servaddr_len);
     serv_addr.sin_addr.s_addr = inet_addr(argv[1]);
     serv_addr.sin_family = AF_INET;
@@ -58,20 +78,101 @@ int main(int argc, char** argv)
         cout << "connect succeed" << endl;
     }
     
-    //开始读写数据
+    //set_nonblock(connfd); 
+
+//将stdin和服务端连接都放到epoll轮询中，尝试实现同时读写
+    epoll_fd = epoll_create(MAX_EVENTS);
+    cout << epoll_fd << endl;
+    ev.data.fd = connfd;
+    ev.events = EPOLLIN | EPOLLHUP | EPOLLET;
+    epoll_ctl(epoll_fd, EPOLL_CTL_ADD, connfd, &ev);
+
+    ev.data.fd = 0;
+    ev.events = EPOLLIN;
+    epoll_ctl(epoll_fd, EPOLL_CTL_ADD, 0, &ev);
+
+    while(true)
+    {
+        nfds = epoll_wait(epoll_fd, events, MAX_EVENTS, 0);
+        if(nfds < 0)
+            handle_error("epoll failed.\n");
+        for(int i = 0; i < nfds; ++i)
+        {
+//表明标准输入有数据到来
+            if(events[i].data.fd == 0)
+            {
+                //cout << "stdin" << endl;
+                read(0, message_send, MAX_MESSAGE);
+                write(connfd, message_send, sizeof(message_send));
+                memset(message_send, 0, sizeof(message_send));
+            }
+            else if(events[i].data.fd == connfd)
+            {
+                //cout << "from server" << endl;
+                if(events[i].events & EPOLLIN)
+                    read(connfd, message_recv, MAX_MESSAGE);
+                printf("%s", message_recv);
+                memset(message_recv, 0, sizeof(message_recv));
+            }
+            else  
+                handle_error("file descriptor error.\n");
+        }
+    }
+
+
+
+
+
+
+
+
+/*
+//将客户端的读功能用epoll来管理，实现实时读取其他用户信息的功能
+    ev.data.fd = connfd;
+    ev.events = EPOLLIN;
+    epoll_ctl(epoll_fd, EPOLL_CTL_ADD, connfd, &ev);
+    setvbuf(stdin, NULL, _IONBF, 0);
+//开始读写数据
     while(1)
     {
-        memset(message, 0, 10); 
-        //fputs("请输入数据", stdout);
-        fgets(message, MAX_MESSAGE, stdin);
-        if(!strcmp(message, "q\n") || !strcmp(message, "Q\n"))
+        memset(message_send, 0, sizeof(message_send)); 
+        fgets(message_send, MAX_MESSAGE, stdin);
+        if(!strcmp(message_send, "q\n") || !strcmp(message_send, "Q\n"))
             break;
-        write_len = write(connfd, message, MAX_MESSAGE);
-        //cout << "write done, begin block." << endl;
-        bzero(message, MAX_MESSAGE);
-        read_len = read(connfd, message, MAX_MESSAGE);
-        printf("echo from server: %s", message);
+        write_len = write(connfd, message_send, MAX_MESSAGE);
+        printf("me: %s", message_send);
+
+        if((read_len = read(connfd, message_recv, MAX_MESSAGE)) < 0)
+        {
+            if(errno == EAGAIN)
+                continue;
+            perror("read failed.\n");
+            exit(1);
+        }
+        printf("receive from server: %s", message_recv);
+        memset(message_recv, 0, sizeof(message_send));
+        /*
+        nfds = epoll_wait(epoll_fd, events, 10, 0);
+
+        cout << nfds << endl;
+        
+        bzero(message_recv, MAX_MESSAGE);
+        printf("receive from server: %s", message_recv);
+        for(int i = 0; i < nfds; ++i)
+        {
+            if(events[i].events & EPOLLIN)
+            {
+                if((read_len = read(connfd, message_recv, MAX_MESSAGE)) < 0)
+                {
+                    perror("read failed.\n");
+                    exit(1);
+                }
+                printf("receive from server: %s", message_recv);
+            }
+        }
     }
+    */
     close(connfd);
+    close(epoll_fd);
     return 0;
 }
